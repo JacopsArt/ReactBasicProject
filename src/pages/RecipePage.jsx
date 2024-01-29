ed97ab9104eb575ee472a7399e4e90e7241db471
Jermaine <jermaine.aroma@gmail.com>
2024-01-29 05:13:17 +0100

    Added recipePage and Button components without styling

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react'; // Import Chakra UI components
 
 const RecipePage = ({ recipe, onBackButtonClick }) => {
   return (
     <div>
       <h2>{recipe.label}</h2>
       {/* Display other details of the selected recipe */}
-
-      {/* Chakra UI Button */}
-      <Button
-        bgColor="blue.300"
-        color="white"
-        size="sm"
-        onClick={onBackButtonClick}
-        mt={4}
-      >
-        Back
-      </Button>
+      <button onClick={onBackButtonClick}>Back</button>
     </div>
   );
 };
