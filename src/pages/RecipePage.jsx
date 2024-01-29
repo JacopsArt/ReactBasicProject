a11f9358c8ccf9cdfa116f03d43b3c61de5d7400
Jermaine <jermaine.aroma@gmail.com>
2024-01-29 17:58:11 +0100

    Made the RecipePage layout with minor styling.

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,10 +1,6 @@
-// RecipePage.jsx
-
-import React from "react";
 import {
   Box,
   Button,
-  Center,
   Flex,
   Heading,
   Image,
@@ -17,85 +13,150 @@ import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const RecipePage = ({ recipe, onBackButtonClick }) => {
   return (
-    <Center h="100vh">
-      <Box w="60%">
-        <Flex justify="space-between" align="center" mb={4}>
-          <HStack spacing={4}>
-            <Button 
-              onClick={onBackButtonClick}
-              leftIcon={<ArrowBackIcon />}
-              variant="outline"
-            >
-              Back
-            </Button>
-          </HStack>
+    <Flex flexDirection="column" align="center" height="100vh">
+      <Box width="60%" height="100%">
+        <Flex
+          backgroundColor="red.300"
+          justify="space-between"
+          align="center"
+          p="2rem 0 "
+        >
+          <Button
+            onClick={onBackButtonClick}
+            leftIcon={<ArrowBackIcon />}
+            variant="outline"
+          >
+            Back
+          </Button>
         </Flex>
 
-        {/* Banner with image */}
-        <Image src={recipe.image} alt={recipe.label} objectFit="cover" w="100%" h="100px" />
+        <Box className="imageBanner">
+          <Image
+            src={recipe.image}
+            alt={recipe.label}
+            objectFit="cover"
+            w="100%"
+            h="250px"
+          />
+        </Box>
 
-        <Flex justify="space-between" align="center" mt={4}>
-          {/* Centered title */}
-          <Heading>{recipe.label}</Heading>
-        </Flex>
-
-        <Flex h="70vh">
-          {/* Left column */}
-          <VStack w="50%" spacing={4} align="flex-start">
-            <Text>Meal Type: {recipe.mealType}</Text>
-            <Text>Dish Type: {recipe.dishType}</Text>
+        {/* Main content */}
+        <Flex className="leftColumn" flexDirection="row" color="white">
+          <Box flex="1" bg="green.500">
+            <Text>{recipe.mealType}</Text>
+            <Heading>{recipe.label}</Heading>
+            <Text>{recipe.dishType}</Text>
             <Text>Total Cooking Time: {recipe.totalTime} min</Text>
             <Text>Servings: {recipe.servings}</Text>
-            {/* Add more details as needed */}
-          </VStack>
-
-          {/* Right column */}
-          <VStack w="50%" spacing={2} align="flex-start">
-            {/* Health labels */}
-            <HStack w="100%" spacing={1} wrap="wrap">
-              {recipe.healthLabels.map((label) => (
-                <Tag
-                  key={label}
-                  bgColor="purple.200"
-                  color="white"
-                  size="sm"
-                  mt={1}
-                >
-                  {label}
-                </Tag>
-              ))}
-            </HStack>
-
-            <Text>Diet Label: {recipe.dietLabels.join(", ")}</Text>
-            <Text>Cautions: {recipe.cautions.join(", ")}</Text>
-            {/* Ingredients */}
-            <Text>Ingredients:</Text>
+            <Heading as="h4" size="md">
+              Ingredients:
+            </Heading>
             <VStack align="flex-start" spacing={1}>
               {recipe.ingredients.map((ingredient, index) => (
                 <Text key={index}>{ingredient.text}</Text>
               ))}
             </VStack>
-
-            <Text>
-              Total Nutrients:
-              <br />
-              Energy: {recipe.totalNutrients.energy} kcal
-              <br />
-              Protein: {recipe.totalNutrients.protein} g
-              <br />
-              Fat: {recipe.totalNutrients.fat} g
-              <br />
-              Carbs: {recipe.totalNutrients.carbs} g
-              <br />
-              Cholesterol: {recipe.totalNutrients.cholesterol} mg
-              <br />
-              Sodium: {recipe.totalNutrients.sodium} mg
-            </Text>
-            {/* Add more details as needed */}
-          </VStack>
+          </Box>
+          <Box className="rightColumn" flex="1" bg="tomato">
+            <Box className="healthLabels">
+              <Text>Health labels</Text>
+              <HStack wrap="wrap">
+                {recipe.healthLabels.map((label) => (
+                  <Tag
+                    key={label}
+                    bgColor="purple.200"
+                    color="white"
+                    fontSize="lg"
+                    mt={1}
+                  >
+                    {label}
+                  </Tag>
+                ))}
+              </HStack>
+            </Box>
+            <Box className="dietLabels">
+              <Text>Diet:</Text>
+              <HStack wrap="wrap">
+                {recipe.dietLabels.map((label) => (
+                  <Tag
+                    key={label}
+                    bgColor="green.200"
+                    color="white"
+                    fontSize="lg"
+                    mt={1}
+                  >
+                    {label}
+                  </Tag>
+                ))}
+              </HStack>
+            </Box>
+            <Box className="cautions">
+              <Text>Cautions:</Text>
+              <HStack wrap="wrap">
+                {recipe.cautions.map((caution) => (
+                  <Tag
+                    key={caution}
+                    bgColor="red.200"
+                    color="white"
+                    fontSize="lg"
+                    mt={1}
+                  >
+                    {caution}
+                  </Tag>
+                ))}
+              </HStack>
+            </Box>
+            <Box className="totalNutrients">
+              <Text>Total Nutrients:</Text>
+              <HStack align="flex-start" spacing={8}>
+                <Flex direction="column">
+                  <Text>
+                    {Math.round(recipe.totalNutrients?.ENERC_KCAL?.quantity)}{" "}
+                    {recipe.totalNutrients?.ENERC_KCAL?.unit}
+                  </Text>
+                  <Text>Energy</Text>
+                </Flex>
+                <Flex direction="column">
+                  <Text>
+                    {Math.round(recipe.totalNutrients?.PROCNT?.quantity)}{" "}
+                    {recipe.totalNutrients?.PROCNT?.unit}
+                  </Text>
+                  <Text>Protein</Text>
+                </Flex>
+                <Flex direction="column">
+                  <Text>
+                    {Math.round(recipe.totalNutrients?.FAT?.quantity)}{" "}
+                    {recipe.totalNutrients?.FAT?.unit}
+                  </Text>
+                  <Text>Fat</Text>
+                </Flex>
+                <Flex direction="column">
+                  <Text>
+                    {Math.round(recipe.totalNutrients?.CHOCDF?.quantity)}{" "}
+                    {recipe.totalNutrients?.CHOCDF?.unit}
+                  </Text>
+                  <Text>Carbs</Text>
+                </Flex>
+                <Flex direction="column">
+                  <Text>
+                    {Math.round(recipe.totalNutrients?.CHOLE?.quantity)}{" "}
+                    {recipe.totalNutrients?.CHOLE?.unit}
+                  </Text>
+                  <Text>Cholesterol</Text>
+                </Flex>
+                <Flex direction="column">
+                  <Text>
+                    {Math.round(recipe.totalNutrients?.NA?.quantity)}{" "}
+                    {recipe.totalNutrients?.NA?.unit}
+                  </Text>
+                  <Text>Sodium</Text>
+                </Flex>
+              </HStack>
+            </Box>
+          </Box>
         </Flex>
       </Box>
-    </Center>
+    </Flex>
   );
 };
 
